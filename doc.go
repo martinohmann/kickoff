bfa97084099f9f970c8056a66c1cf203608c7711
martinohmann <martinohmann@gmail.com>
2020-01-11 09:03:34 +0100

    fix pkg name

--- doc.go
+++ doc.go
@@ -1,8 +1,8 @@
-// Package skeleton provides a CLI tool for bootstrapping projects from
-// skeleton directories. The skeletons can contain any kind of file and special
-// *.skel files which will be evaluated using the golang template engine.
+// Package kickoff provides a CLI tool for bootstrapping projects from skeleton
+// directories. The skeletons can contain any kind of files and special *.skel
+// files which will be evaluated using the golang template engine.
 //
 // The tool was initial meant for bootstrapping golang projects, but it is
 // actually language agnostic as the skeletons do not need to be golang
 // specific.
-package skeleton
+package kickoff
