35d6cd0ebf43708162e986b86caa0d59bc3dd31a
martinohmann <martinohmann@gmail.com>
2020-01-19 13:04:11 +0100

    add zsh and bash completion

---
+++ pkg/cmd/completion.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/martinohmann/kickoff/pkg/cli"
+	"github.com/martinohmann/kickoff/pkg/cmdutil"
+	"github.com/spf13/cobra"
+)
+
+func NewCompletionCmd(streams cli.IOStreams) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "completion",
+		Short: "Shell completion",
+		Long: cmdutil.LongDesc(`
+			Configure your shell to load kickoff completions.
+		`),
+		Example: cmdutil.Examples(`
+			# Add to your ~/.bashrc for bash completion:
+			. <(kickoff completion bash)
+
+			# Add to your ~/.zshrc for zsh completion:
+			. <(kickoff completion zsh)
+		`),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			shell := args[0]
+			rootCmd := cmd.Root()
+
+			switch shell {
+			case "bash":
+				return rootCmd.GenBashCompletion(streams.Out)
+			case "zsh":
+				err := rootCmd.GenZshCompletion(streams.Out)
+				if err != nil {
+					return err
+				}
+
+				fmt.Fprintln(streams.Out, "compdef _kickoff kickoff")
+			default:
+				return fmt.Errorf("unsupported shell: %s", shell)
+			}
+			return nil
+		},
+	}
+
+	return cmd
+}
