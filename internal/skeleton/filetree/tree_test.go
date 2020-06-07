3a27677586df55b383db94ab3519ca88a507c545
martinohmann <martinohmann@gmail.com>
2020-06-07 13:18:41 +0200

    improve skeleton file tree output (#64)
    This fixes the skeleton file tree output. Previously, the `kickoff
    skeleton show` command printed a flat list of filenames where only the
    first level resembled a tree like structure which looked like this:
    
    Files           golang/cli
                    └── .gitignore.skel
                    └── .golangci.yml
                    └── .travis.yml.skel
                    └── Makefile.skel
                    └── README.md.skel
                    └── cmd
                    └── cmd/{{.Project.Name}}
                    └── cmd/{{.Project.Name}}/main.go.skel
                    └── doc.go.skel
                    └── go.mod.skel
                    └── pkg
                    └── pkg/version
                    └── pkg/version/version.go
    
    The new implementation will produce tree which looks like this:
    
    Files           golang/cli
                    ├── .gitignore.skel
                    ├── .golangci.yml
                    ├── .travis.yml.skel
                    ├── Makefile.skel
                    ├── README.md.skel
                    ├── cmd/
                    │   └── {{.Project.Name}}/
                    │       └── main.go.skel
                    ├── doc.go.skel
                    ├── go.mod.skel
                    └── pkg/
                        └── version/
                            └── version.go
    
    It also colors directories and *.skel files to make them stand out a
    little. Also the file list is now printed as the last item.

---
+++ internal/skeleton/filetree/tree_test.go
@@ -0,0 +1,35 @@
+package filetree
+
+import (
+	"testing"
+
+	"github.com/martinohmann/kickoff/internal/skeleton"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuild(t *testing.T) {
+	s := &skeleton.Skeleton{
+		Info: &skeleton.Info{Name: "my/skeleton"},
+		Files: []*skeleton.File{
+			{RelPath: "."},
+			{RelPath: ".kickoff.yaml"},
+			{RelPath: "README.md.skel"},
+			{RelPath: "foo/bar"},
+			{RelPath: "foo/sometemplate.skel"},
+			{RelPath: "foo/{{.Values.filename}}/qux"},
+		},
+	}
+
+	tree := Build(s)
+
+	expected := `my/skeleton
+├── .kickoff.yaml
+├── README.md.skel
+└── foo/
+    ├── bar
+    ├── sometemplate.skel
+    └── {{.Values.filename}}/
+        └── qux`
+
+	assert.Equal(t, expected, tree.Print())
+}
